3ffc75c314880eb0572a3847aedcfad3f2d05869
Ari Atkins <ari@unpluggedinstruments.com>
2012-09-20 12:25:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlossomDatabase::Application.config.secret_token = 'f64bd4ece7926cdade62a1a06fdfd3002dbfa49213ec7e5e635f9f87300f718043c27a836e6c6fa5b08b2c54ddfbe2858707f57e4db0437f5d485d02fff8c85a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BlossomDatabase::Application.config.session_store :cookie_store, key: '_blossom_database_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BlossomDatabase::Application.config.session_store :active_record_store
